e2b70c6000478a94be8b24c6bb9f4ed6bf5abe2b
이예슬 <20152917@sungshin.ac.kr>
2021-11-01 01:59:54 +0900

    feat : spring cloud gateway 실습용 서버1

--- second-service/src/main/kotlin/com/example/secondservice/controller/SecondServiceController.kt
+++ second-service/src/main/kotlin/com/example/secondservice/controller/SecondServiceController.kt
@@ -1,4 +1,15 @@
 package com.example.secondservice.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/second-service")
 class SecondServiceController {
+
+    @GetMapping("/welcome")
+    fun welcome(): String {
+        return "Welcome to Second Service"
+    }
 }
